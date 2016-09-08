5389d9abacd99eda54f882d22b5d566f2bac644d
sneha <snehasne517@gmail.com>
2016-09-08 11:26:05 +0530

    test program using json

---
+++ Sept8/services/IService1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace facebookfinal.Views.Home
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        void DoWork();
+        [OperationContract]
+        string Login();
+        [OperationContract]
+
+    
+        [DataContract]
+        public class Logginuser
+        {
+            public string Firstname { get; set; }
+            public string Lastname { get; set; }
+            public string Profilepic { get; set; }
+            public string Password { get; set; }
+            public string Username { get; set; }
+            public string LoginID { get; set; }
+            public string Message { get; set; }
+
+        }
+        
+
+    }
+}
---
+++ Sept8/services/Service1.svc.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Data.SqlClient;
+using Newtonsoft.Json;
+namespace facebookfinal.Views.Home
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    public class Service1 : IService1
+    {
+        public void DoWork()
+        {
+        }
+        public string Login()
+        {
+            SqlConnection con = new SqlConnection("Data Source=KUTTYCHATTAN-PC;Integrated Security=True");
+            string str;
+            con.Open();
+            str = "select * from  Userlogin";
+            SqlCommand cmd = con.CreateCommand();
+          //  cmd.Dispose();
+            //con.Close();
+            SqlDataReader rdr=cmd.ExecuteReader();
+            Logginuser logg =new Logginuser();
+            while(rdr.Read())
+            {
+                if(((logg.LoginID.Equals(rdr["LoginID"].ToString()))&&(((logg.Password.Equals(rdr["Password"].ToString())))
+                {     
+                    logg.LoginID=rdr["LoginID"].ToString();
+                    logg.Password=rdr["Password"].ToString();
+
+                }
+            }
+
+
+           
+        
+        }  
+    }
+
